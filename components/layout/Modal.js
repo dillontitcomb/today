28d35de5c046c994aa96bb8c4ac1ac21712d2d91
Dillon Titcomb <dillontitcomb@gmail.com>
2021-03-18 19:17:13 -0500

    add modal functionality and modal component.

---
+++ components/layout/Modal.js
@@ -0,0 +1,79 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const ModalBackground = styled.div`
+  margin: auto;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContainer = styled.div`
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+  padding: 2rem;
+`;
+
+const CloseModalButton = styled.button`
+  cursor: pointer;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  width: 24px;
+  height: 24px;
+  padding: 0;
+  z-index: 10;
+  border: none;
+  background-color: #fff;
+`;
+
+export default function Modal(props) {
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      props.setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === 'Escape' && props.showModal === true) {
+        props.setShowModal(false);
+      }
+    },
+    [props.setShowModal, props.showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+    return () => document.removeEventListener('keydown', keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {props.showModal && (
+        <ModalBackground ref={modalRef} onClick={closeModal}>
+          <ModalContainer>
+            {props.children}
+            <CloseModalButton
+              onClick={() => props.setShowModal((prev) => !prev)}
+            >
+              X
+            </CloseModalButton>
+          </ModalContainer>
+        </ModalBackground>
+      )}
+    </>
+  );
+}
--- pages/habits/index.js
+++ pages/habits/index.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import AddHabit from '../../components/habits/AddHabit';
+import { Button } from '../../components/layout/Buttons';
+import Modal from '../../components/layout/Modal';
+import { Container } from '../../components/layout/Wrappers';
 
-export default function habits(params) {
+export default function habits() {
+  // TODO: Create UseModal hook!
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    console.log('Trying to display modal.');
+    setShowModal((prev) => !prev);
+  };
+
   return (
     <div>
       <h1>Habits Page</h1>
       <AddHabit></AddHabit>
+      <Container center>
+        <Button onClick={openModal}>Show Modal</Button>
+        {showModal && (
+          <Modal showModal={showModal} setShowModal={setShowModal}>
+            Here's the modal content
+          </Modal>
+        )}
+      </Container>
     </div>
   );
 }

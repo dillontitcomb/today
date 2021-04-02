d68b21bd44aaa7c7c2224f382d36d9d961b1d350
Dillon Titcomb <dillontitcomb@gmail.com>
2021-04-02 12:23:28 -0500

    complete simpleAddHabit form, need to finish simpleAddTask form to allow for adding tasks to new habit.

--- components/habits/SimpleAddHabit.js
+++ components/habits/SimpleAddHabit.js
@@ -6,6 +6,9 @@ import { fetcher } from '../../utils/helperFunctions';
 import { Button, OutlineButton } from '../layout/Buttons';
 import { Form, NumberInput, Option, Select, TextInput } from '../layout/Forms';
 import { SubText } from '../layout/Typography';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+
 // Fields to include/add:
 // name, tasks, startDate, endDate, frequencyValue, frequencyPeriod
 
@@ -15,17 +18,20 @@ const FormGroup = styled.div`
 const FormContainer = styled.div`
   margin: auto;
   text-align: left;
-  max-width: 500px;
+  max-width: 600px;
 `;
 
 const DateContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  text-align: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 0rem 1rem;
 `;
 
 export default function SimpleAddHabit() {
   const [habitPeriod, setHabitPeriod] = useState('day');
+  const [showTaskForm, setShowTaskForm] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -38,19 +44,31 @@ export default function SimpleAddHabit() {
     },
 
     onSubmit: async (values) => {
-      const newHabit = await fetcher('/api/habits', {
-        method: 'POST',
-        body: JSON.stringify(values),
-      });
-      let habit = newHabit.data;
-      console.log(`New habit, ${habit.name} added`);
-      mutate('/api/habits');
+      console.log('Trying to submit!');
+      console.log(values);
+
+      // const newHabit = await fetcher('/api/habits', {
+      //   method: 'POST',
+      //   body: JSON.stringify(values),
+      // });
+      // let habit = newHabit.data;
+      // console.log(`New habit, ${habit.name} added`);
+      // mutate('/api/habits');
     },
   });
   function onSelect(e) {
     e.preventDefault();
     formik.setFieldValue(e.target.name, e.target.value);
     if (e.target.name === 'frequencyPeriod') setHabitPeriod(e.target.value);
+  }
+  function handleStartDateChange(selectedDay) {
+    formik.setFieldValue('startDate', selectedDay);
+  }
+  function handleEndDateChange(selectedDay) {
+    formik.setFieldValue('endDate', selectedDay);
+  }
+  function handleShowTaskForm() {
+    setShowTaskForm(!showTaskForm);
   }
 
   return (
@@ -71,16 +89,25 @@ export default function SimpleAddHabit() {
           </FormGroup>
           <DateContainer>
             <FormGroup>
-              <h3>Start Date</h3>
-              
+              <label>Start Date </label>
+              <DayPickerInput
+                onDayChange={handleStartDateChange}
+                selectedDays={formik.values.startDate}
+              />
+              <SubText>When will you start?</SubText>
             </FormGroup>
             <FormGroup>
-              <h3>End Date</h3>
+              <label>End Date </label>
+              <DayPickerInput
+                onDayChange={handleEndDateChange}
+                selectedDays={formik.values.endDate}
+              />
+              <SubText>When will you finish?</SubText>
             </FormGroup>
           </DateContainer>
 
           <FormGroup>
-            <label htmlFor='frequencyPeriod'>Tracking Period</label>
+            <label htmlFor='frequencyPeriod'>Period</label>
             <Select
               name='frequencyPeriod'
               id='frequencyPeriod'
@@ -90,30 +117,34 @@ export default function SimpleAddHabit() {
               <Option value='week'>Weekly</Option>
               <Option value='month'>Monthly</Option>
             </Select>
-            <SubText>
-              Over what time period do you want to monitor this habit?
-            </SubText>
+            <SubText>How often will you engage in this habit?</SubText>
           </FormGroup>
           <FormGroup>
-            <label htmlFor='frequencyValue'>Tracking Frequency</label>
-            <NumberInput
+            <label htmlFor='frequencyValue'>Frequency</label>
+            <Select
               name='frequencyValue'
               id='frequencyValue'
-              onChange={formik.handleChange}
-              value={formik.values.frequencyValue}
-            ></NumberInput>
+              onChange={onSelect}
+            >
+              <Option value='1'>Once</Option>
+              <Option value='2'>Twice</Option>
+              <Option value='3'>3x</Option>
+              <Option value='4'>4x</Option>
+              <Option value='5'>5x</Option>
+              <Option value='6'>6x</Option>
+              <Option value='7'>7x</Option>
+            </Select>
             <SubText>How many times per {habitPeriod}?</SubText>
           </FormGroup>
           {/* TODO: Add Logic for adding tasks to habit   */}
           {/* TODO: Add Modal! */}
-          <Button buttonstyle='primary' noradius>
-            Add New Task
-          </Button>
-          <Button noradius>Select Existing</Button>
-          <br></br>
-          <OutlineButton noradius buttonstyle='secondary' type='submit'>
+
+          <Button onClick={handleShowTaskForm}>Add Tasks</Button>
+
+          {/* <OutlineButton buttonstyle='secondary' type='submit'>
             Create New Habit
-          </OutlineButton>
+          </OutlineButton> */}
+          {showTaskForm && <h1>Here's the task form!</h1>}
         </FormContainer>
       </Form>
     </div>

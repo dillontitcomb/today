3fe3223f525a8b303eae0f4fa10d5b0c8e4f255e
Dillon Titcomb <dillontitcomb@gmail.com>
2021-07-09 18:02:08 -0500

    make checkbox for tasks list larger.

--- components/tasks/tasksList/detailedTask/DetailedTaskComplete.js
+++ components/tasks/tasksList/detailedTask/DetailedTaskComplete.js
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 import { useState } from 'react';
 const Container = styled.div`
   place-self: end;
+  display: flex;
+  height: 100%;
+  align-items: center;
 `;
-const Checkbox = styled.input``;
+const Checkbox = styled.input`
+  height: 1.2rem;
+  width: 1.2rem;
+`;
 // TODO: make checkbox a complete button
 
 export default function DetailedTaskComplete({ task }) {
